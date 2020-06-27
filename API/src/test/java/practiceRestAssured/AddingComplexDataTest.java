375d0ff7d0447789d1f36b25b42b8f76be9a7c7d
muthhuSelenium <62168171+muthhuSelenium@users.noreply.github.com>
2020-06-27 15:53:39 +0530

    Update and rename AddingComplexData.java to AddingComplexDataTest.java

--- API/src/test/java/practiceRestAssured/AddingComplexDataTest.java
+++ API/src/test/java/practiceRestAssured/AddingComplexDataTest.java
@@ -11,7 +11,7 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSender;
 import io.restassured.specification.RequestSpecification;
 
-public class AddingComplexData {
+public class AddingComplexDataTest {
 	
 	@Test
 	public void addComplexData()
